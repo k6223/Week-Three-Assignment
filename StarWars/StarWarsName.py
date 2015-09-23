ea5a7688aeafb32b7d28c8a7a20f2c484d6c9c44
k6223 <kollin.schalhamer@myemail.indwes.edu>
2015-09-23 18:53:57 +0000

    StarWarsName.py updated

--- StarWars/StarWarsName.py
+++ StarWars/StarWarsName.py
@@ -1,8 +1,8 @@
-# name = "Kollin"
+
 #CIS-125-82A
 #StarWarsName.py
 #
-#Creates you a StarWars name
+#Creates you a StarWars first and last name
 
 #Write program code here
 
@@ -11,5 +11,11 @@
 #Process
 
 #Output
+print("enter your StarWars name")
+lastName = input("please enter your last name: ")
+firstName = input("please enter your first name: ")
+motherName = input("please enter your mother's maiden name: ")
+birthTown = input("please enter your birth town: ")
 
 
+print(firstName[0:2] + lastName[0:3], motherName[0:2] + birthTown[0:3])
